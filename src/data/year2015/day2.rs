7ee6c26be4a0375a32cdb7843a4d4274e240b9d8
Pawel Motyl <pawmot@gmail.com>
2022-12-04 12:01:14 +0100

    y22d3 improved implementation

--- src/data/year2015/day2.rs
+++ src/data/year2015/day2.rs
@@ -1,16 +1,9 @@
 use crate::data::common::{read_lines, DatasetType};
 use std::io;
 
+// TODO: investigate ways to make this simpler, possibly do the collection to io::Result in read_lines
 pub fn get_data(dataset_type: DatasetType) -> io::Result<Vec<String>> {
     let lines = read_lines((2015, 2), dataset_type)?;
-
-    let mut result = vec![];
-
-    for line_res in lines {
-        let line = line_res?;
-        result.push(line);
-    }
-
-    result.pop();
-    return Ok(result);
+    Ok(lines.into_iter().collect::<io::Result<Vec<_>>>()?
+        .into_iter().filter(|s| !s.is_empty()).collect::<Vec<_>>())
 }
--- src/year2022/day3.rs
+++ src/year2022/day3.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod day3 {
-    use std::{collections::{HashMap, HashSet}, io};
+    use std::{collections::HashSet, io};
 
     use crate::data::{
         common::DatasetType::{FULL, SAMPLE},
@@ -11,15 +11,18 @@ mod day3 {
         data.into_iter()
             .map(|s| s.chars().collect::<Vec<_>>())
             .map(|v| {
-                let split = v.split_at(v.len()/2);
+                let split = v.split_at(v.len() / 2);
                 (split.0.to_vec(), split.1.to_vec())
             })
             .flat_map(|(l, r)| {
                 let mut left_set = HashSet::new();
                 let mut right_set = HashSet::new();
-                l.iter().for_each(|c| drop(left_set.insert(*c)));
-                r.iter().for_each(|c| drop(right_set.insert(*c)));
-                left_set.intersection(&right_set).map(|c| *c).collect::<Vec<_>>()
+                l.into_iter().for_each(|c| drop(left_set.insert(c)));
+                r.into_iter().for_each(|c| drop(right_set.insert(c)));
+                left_set
+                    .intersection(&right_set)
+                    .copied()
+                    .collect::<Vec<_>>()
             })
             .map(|c| {
                 if c.is_ascii_lowercase() {
@@ -48,36 +51,27 @@ mod day3 {
     }
 
     fn b(data: Vec<String>) -> u32 {
-        let mut it = data.into_iter();
-        let mut result = 0u32;
-
-        // find a more elegant way to group items into groups of n elements
-        loop {
-            let next = it.next();
-
-            if next.is_none() {
-                break;
-            }
-
-            let one = next.unwrap();
-            let two = it.next().unwrap();
-            let three = it.next().unwrap();
-
-            let one_set = one.chars().collect::<HashSet<_>>();
-            let two_set = two.chars().collect::<HashSet<_>>();
-            let three_set = three.chars().collect::<HashSet<_>>();
-
-            let one_two_set = one_set.intersection(&two_set).map(|c| *c).collect::<HashSet<_>>();
-            let auth = one_two_set.intersection(&three_set).map(|c| *c).collect::<Vec<_>>()[0];
-
-            result += if auth.is_ascii_lowercase() {
-                (auth as u32) - 96
-            } else {
-                (auth as u32) - 38
-            }
-        }
-
-        return result;
+        data.chunks(3)
+            .into_iter()
+            .map(|ch| {
+                ch.into_iter()
+                    .map(|s| s.chars().collect::<HashSet<_>>())
+                    .collect::<Vec<_>>()
+            })
+            .map(|sets| {
+                sets.into_iter()
+                    .reduce(|acc, next| acc.intersection(&next).copied().collect::<HashSet<_>>())
+                    .unwrap()
+            })
+            .map(|set| set.into_iter().next().unwrap())
+            .map(|auth| {
+                if auth.is_ascii_lowercase() {
+                    (auth as u32) - 96
+                } else {
+                    (auth as u32) - 38
+                }
+            })
+            .sum()
     }
 
     #[test]
